ef4323d2cee50cfca6a14453c9534c6b00ced400
nyoshimizu <nyoshimizu@gmail.com>
2016-05-11 10:42:12 -0700

    Added class to access and query verilog SQL database.

--- db/loadverilog.py
+++ db/loadverilog.py
@@ -5,14 +5,15 @@ Load verilog code and return its contents as a verilog db class, defined below.
 import mmap
 from db.gate_db import GateDB
 from db.gates import Gates
+from sqlalchemy import *
 
 
 class VerilogDB:
 
     def __init__(self):
-        self.input_pins = {}
-        self.output_pins = {}
-        self.node_pins = {}
+        self.input_pins = set()
+        self.output_pins = set()
+        self.node_pins = set()
         self.gates = GateDB()
 
     def input_pins_sorted(self):
@@ -26,6 +27,13 @@ class VerilogDB:
 
 
 def load_verilog(circuit):
+    """
+    Load verilog file and return as VerilogDB object.
+
+    circuit is name of circuit to load
+
+    Returns VerilogDB object
+    """
 
     filename = "../verilog/" + circuit + ".v"
 
@@ -96,3 +104,117 @@ def load_verilog(circuit):
     file_verilog.close()
 
     return return_db
+
+
+class VerilogSQL:
+    """
+    Class to access and query verilog SQL database.
+    """
+
+    def __init__(self):
+        self.loaded = False
+        self.circuit = None
+        self.conn = None
+        self.db = VerilogDB()
+
+    def loadfile(self):
+        engine = create_engine('sqlite:///verilog.sqlite3', echo=False)
+        self.conn = engine.connect()
+        self.loaded = True
+
+    def closefile(self):
+        self.conn.close()
+        self.loaded = False
+
+    def loadinputpins(self, circuit):
+        self.circuit = circuit
+
+        metadata = MetaData()
+
+        input_pins_table = Table('input_pins', metadata,
+                                 Column('id', INTEGER, primary_key=True),
+                                 Column('circuit', TEXT),
+                                 Column('input_pin', TEXT)
+                                 )
+
+        sel = select([input_pins_table]).where(input_pins_table.c.circuit == self.circuit)
+
+        query = self.conn.execute(sel)
+
+        self.db.input_pins = set()
+        for _, _, pin in query:
+            self.db.input_pins.add(pin)
+
+        query.close()
+
+    def loadoutputpins(self, circuit):
+        self.circuit = circuit
+
+        metadata = MetaData()
+
+        output_pins_table = Table('output_pins', metadata,
+                                  Column('id', INTEGER, primary_key=True),
+                                  Column('circuit', TEXT),
+                                  Column('output_pin', TEXT)
+                                  )
+
+        sel = select([output_pins_table]).where(output_pins_table.c.circuit == self.circuit)
+
+        query = self.conn.execute(sel)
+
+        self.db.output_pins = set()
+        for _, _, pin in query:
+            self.db.output_pins.add(pin)
+
+        query.close()
+
+    def loadnodepins(self, circuit):
+
+        self.circuit = circuit
+
+        metadata = MetaData()
+
+        node_pins_table = Table('node_pins', metadata,
+                                Column('id', INTEGER, primary_key=True),
+                                Column('circuit', TEXT),
+                                Column('node_pin', TEXT)
+                                )
+
+        sel = select([node_pins_table]).where(node_pins_table.c.circuit == self.circuit)
+
+        query = self.conn.execute(sel)
+
+        self.db.node_pins = set()
+        for _, _, pin in query:
+            self.db.node_pins.add(pin)
+
+        query.close()
+
+    def loadgates(self, circuit):
+
+        self.circuit = circuit
+
+        metadata = MetaData()
+
+        gates_table = Table('gates', metadata,
+                            Column('id', INTEGER, primary_key=True),
+                            Column('circuit', TEXT),
+                            Column('gate', TEXT),
+                            Column('output_pin', TEXT),
+                            Column('input_pin', TEXT)
+                            )
+
+        sel = select([gates_table]).where(gates_table.c.circuit == self.circuit)
+
+        query = self.conn.execute(sel)
+
+        self.db.gates.db = {}
+        for _, _, gate, out_pin, in_pin in query:
+            if out_pin in self.db.gates.db:
+                in_pin_other = self.db.gates.db[out_pin].input_pin
+                self.db.gates.db[out_pin] = GateDB.GateElement(gate, out_pin, [in_pin]+[in_pin_other])
+            else:
+                self.db.gates.db[out_pin] = GateDB.GateElement(gate, out_pin, in_pin)
+
+        query.close()
+
