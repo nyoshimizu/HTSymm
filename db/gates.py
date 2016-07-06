9d79cc46a0ec87730171dffef205b2924f3caedc
nyoshimizu <nyoshimizu@gmail.com>
2016-07-06 13:39:45 -0700

    Move path_delay to GateDB, PEP8

--- db/gates.py
+++ db/gates.py
@@ -1,5 +1,6 @@
 """
-Class definition for gates. Contains constants for gate names, gate delays as a function of transistor delays,
+Class definition for gates. Contains constants for gate names, gate delays as a
+function of transistor delays,
 """
 
 
@@ -10,3 +11,14 @@ class Gates:
         self.names = {'and', 'nand', 'or', 'nor', 'not', 'xor', 'buf'}
         self.delays = {'nand': 1, 'nor': 1, 'xor': 1, 'not': 1,
                                   'and': 1, 'or': 1, 'buf': 1}
+
+    def path_delay(self, gate_list):
+
+        try:
+            delays = [self.delays[gate] for gate in gate_list]
+            delays = sum(delays)
+
+        except ValueError:
+            print('Error in gate list received for calculating path delay')
+
+        return delays
