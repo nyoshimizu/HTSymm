3e011feefc773810e3866fb684647e9f514edf6d
nyoshimizu <nyoshimizu@gmail.com>
2016-07-06 15:24:21 -0700

    Load gates as single entries, rather than per input pin.

--- db/convert.py
+++ db/convert.py
@@ -1,10 +1,24 @@
 """
-Convert text file databases into SQLite databases to be used by all other modules. Uses SQLite3 via SQLAlchemy.
+Convert text file databases into SQLite databases to be used by all other
+modules. Uses SQLite3 via SQLAlchemy.
 """
 
-from sqlalchemy import *
-from db.loadverilog import *
-from db.gates import *
+from mmap import mmap
+from db.loadverilog import (
+    load_verilog,
+    VerilogSQL
+)
+from db.gates import Gates
+from sqlalchemy import (
+    create_engine,
+    MetaData,
+    select,
+    Table,
+    Column,
+    Integer,
+    String,
+)
+
 
 def convert_verilog_SQLite(circuit):
     """
@@ -12,58 +26,44 @@ def convert_verilog_SQLite(circuit):
 
     circuit is circuit to be converted.
 
-    Table input_pins (id INTEGER, circuit TEXT, input_pin TEXT)
-    Table output_pins (id INTEGER, circuit TEXT, output_pin TEXT)
-    Table node_pins (id INTEGER, circuit TEXT, node_pin TEXT)
-    Table gates (id INTEGER, circuit TEXT, output_pin TEXT, gate TEXT, input_pin TEXT)
+    Table input_pins_table (id Integer, circuit String, input_pin String)
+    Table output_pins_table (id Integer, circuit String, output_pin String)
+    Table node_pins_table (id Integer, circuit String, node_pin String)
+    Table gates_table (
+                 id Integer, circuit String, output_pin String, gate String,
+                 input_pin0 String, input_pin1 String, input_pin2 String,
+                 input_pin3 String, input_pin4 String, input_pin5 String,
+                 input_pin6 String, input_pin7 String, input_pin8 String,
+                 input_pin9 String
+    )
 
     Returns 1 if data inserted
     Returns 0 if data already exists
     """
 
+    VerilogSQLdb = VerilogSQL()
+
     verilog_db = load_verilog(circuit)
 
     engine = create_engine('sqlite:///verilog.sqlite3', echo=False)
 
-    metadata = MetaData()
+    VerilogSQLdb.metadata.create_all(engine)
 
-    input_pins = Table('input_pins', metadata,
-                       Column('id', INTEGER, primary_key=True),
-                       Column('circuit', TEXT),
-                       Column('input_pin', TEXT)
-                       )
-
-    output_pins = Table('output_pins', metadata,
-                        Column('id', INTEGER, primary_key=True),
-                        Column('circuit', TEXT),
-                        Column('output_pin', TEXT)
-                        )
-
-    node_pins = Table('node_pins', metadata,
-                      Column('id', INTEGER, primary_key=True),
-                      Column('circuit', TEXT),
-                      Column('node_pin', TEXT)
-                      )
-
-    gates = Table('gates', metadata,
-                  Column('id', INTEGER, primary_key=True),
-                  Column('circuit', TEXT),
-                  Column('gate', TEXT),
-                  Column('output_pin', TEXT),
-                  Column('input_pin', TEXT)
-                  )
-
-    metadata.create_all(engine)
     conn = engine.connect()
 
     # Check if circuit is already in SQLite database input_pins table
-    s = select([input_pins.c.circuit]).distinct()
-    result = conn.execute(s).fetchall()
-    result = str(result)
+    try:
+        s = select([VerilogSQLdb.input_pins_table.c.circuit]).distinct()
+        result = conn.execute(s).fetchall()
+        result = str(result)
 
-    if circuit in result:
-        conn.close()
-        return 0
+        if circuit in result:
+            conn.close()
+            return 0
+
+    except:
+        print('Error from SQLAlchemy while checking for existence of tables.')
+        # return -1
 
     # Write SQL database from verilog_db
 
@@ -75,7 +75,7 @@ def convert_verilog_SQLite(circuit):
                         'input_pin': pin
                         }]
 
-    conn.execute(input_pins.insert(), insert_SQL)
+    conn.execute(VerilogSQLdb.input_pins_table.insert(), insert_SQL)
 
     output_pins_sorted = verilog_db.output_pins_sorted()
 
@@ -85,7 +85,7 @@ def convert_verilog_SQLite(circuit):
                         'output_pin': pin
                         }]
 
-    conn.execute(output_pins.insert(), insert_SQL)
+    conn.execute(VerilogSQLdb.output_pins_table.insert(), insert_SQL)
 
     node_pins_sorted = verilog_db.node_pins_sorted()
 
@@ -95,18 +95,29 @@ def convert_verilog_SQLite(circuit):
                         'node_pin': pin
                         }]
 
-    conn.execute(node_pins.insert(), insert_SQL)
+    conn.execute(VerilogSQLdb.node_pins_table.insert(), insert_SQL)
 
     insert_SQL = []
     for pin in node_pins_sorted + output_pins_sorted:
-        for inpin in verilog_db.gates.db[pin].input_pin:
-            insert_SQL += [{'circuit': circuit,
-                            'gate': verilog_db.gates.db[pin].gate,
-                            'output_pin': pin,
-                            'input_pin': inpin
-                            }]
 
-    conn.execute(gates.insert(), insert_SQL)
+        temp_SQL = {}
+        for k, inpin in enumerate(verilog_db.gatedb.db[pin].input_pin):
+            temp_SQL['input_pin'+str(k)] = inpin
+
+        temp_SQL.update({'circuit': circuit,
+                         'gate': verilog_db.gatedb.db[pin].gate,
+                         'output_pin': pin
+                         })
+
+        insert_SQL += [temp_SQL]
+
+    # Only SQL Insert's of the same size can be batch inserted.
+    minsize = min([len(entry) for entry in insert_SQL])
+    maxsize = max([len(entry) for entry in insert_SQL])
+
+    for k in range(minsize, maxsize+1):
+        temp_SQL = [entry for entry in insert_SQL if len(entry) is k]
+        conn.execute(VerilogSQLdb.gates_table.insert(), temp_SQL)
 
     conn.close()
     return 1
@@ -124,44 +135,59 @@ def convert_verilog_SQLite(circuit):
 # convert_verilog_SQLite('c6288')
 # convert_verilog_SQLite('c7552')
 
+# This database renamed verilog_bak.sqlite3, 7/6/2016. DB changed so that
+# gates_table lists all input pins in same db entry rather than as separate
+# entries each.
+# convert_verilog_SQLite('c17')
+# convert_verilog_SQLite('c432')
+# convert_verilog_SQLite('c499')
+# convert_verilog_SQLite('c880a')
+# convert_verilog_SQLite('c1355')
+# convert_verilog_SQLite('c1908')
+# convert_verilog_SQLite('c2670')
+# convert_verilog_SQLite('c3540')
+# convert_verilog_SQLite('c5315')
+# convert_verilog_SQLite('c6288')
+# convert_verilog_SQLite('c7552')
+
 
 def convert_symmpaths_SQLite(circuit):
     """
-    Convert symmpath text data into SQLite. Due to size of symmpath files, serial read text file and write
-    into SQLite file.
+    Convert symmpath text data into SQLite. Due to size of symmpath files,
+    serial read text file and write into SQLite file.
 
     circuit is circuit to be converted.
 
-    Table gate_list (id INTEGER, circuit TEXT, symmpath_id INTEGER, gate_list TEXT, path_delay INTEGER)
-    Table path_list (id INTEGER, circuit TEXT, symmpath_id INTEGER, path_list TEXT)
+    Table gate_list (id Integer, circuit String, symmpath_id Integer,
+                     gate_list String, path_delay Integer)
+    Table path_list (id Integer, circuit String, symmpath_id Integer,
+                     path_list String)
 
     Returns 1 if data inserted
     Returns 0 if data already exists
     """
-
-    symmpath_db = load_verilog(circuit)
 
     engine = create_engine('sqlite:///symmpath.sqlite3', echo=False)
 
     metadata = MetaData()
 
     gate_list = Table('gate_list', metadata,
-                      Column('id', INTEGER, primary_key=True),
-                      Column('circuit', TEXT),
-                      Column('symmpath_id', INTEGER),
-                      Column('gate_list', TEXT),
-                      Column('path_delay', INTEGER),
+                      Column('id', Integer, primary_key=True),
+                      Column('circuit', String),
+                      Column('symmpath_id', String),
+                      Column('gate_list', String),
+                      Column('path_delay', Integer),
                       )
 
     path_list = Table('path_list', metadata,
-                      Column('id', INTEGER, primary_key=True),
-                      Column('circuit', TEXT),
-                      Column('symmpath_id', INTEGER),
-                      Column('path_list', TEXT),
-                      Column('input_pin', TEXT),
-                      Column('output_pin', TEXT),
-                      Column('node_pins', TEXT),
-                      Column('all_pins', TEXT)
+                      Column('id', Integer, primary_key=True),
+                      Column('circuit', String),
+                      Column('symmpath_id', String),
+                      Column('path_list', String),
+                      Column('input_pin', String),
+                      Column('output_pin', String),
+                      Column('node_pins', String),
+                      Column('all_pins', String)
                       )
 
     metadata.create_all(engine)
@@ -179,7 +205,10 @@ def convert_symmpaths_SQLite(circuit):
     # Convert symmpath file into SQLite db.
     filename = "../symmpaths/" + circuit + "symmpaths.txt"
     with open(filename, "r") as file_data:
-        file_symmpath = mmap.mmap(file_data.fileno(), 0, access=mmap.ACCESS_READ)
+        file_symmpath = mmap.mmap(file_data.fileno(),
+                                  0,
+                                  access=mmap.ACCESS_READ
+                                  )
 
     gate_line = file_symmpath.readline()
     gate_line = gate_line.decode('ascii')
@@ -211,16 +240,22 @@ def convert_symmpaths_SQLite(circuit):
         insert_gates_SQL = [{'circuit': circuit,
                              'symmpath_id': symmpath_id,
                              'gate_list': gate_line,
-                             'path_delay': Gates().path_delay(gate_line.split(','))
+                             'path_delay':
+                             Gates().path_delay(gate_line.split(','))
                              }]
 
         for path in paths:
             path_temp = path.split(',')
             input_pin = path_temp[-1]
             output_pin = path_temp[0]
-            node_pins = ','.join(sorted(path_temp[1:-1],
-                                        key=lambda number: int(''.join(k for k in number if k.isdigit()))))
-            all_pins = ','.join(sorted(path_temp, key=lambda number: int(''.join(k for k in number if k.isdigit()))))
+            node_pins = ','.join(
+             sorted(path_temp[1:-1],
+                    key=lambda number:
+                    int(''.join(k for k in number if k.isdigit()))))
+            all_pins = ','.join(
+             sorted(path_temp,
+                    key=lambda number:
+                    int(''.join(k for k in number if k.isdigit()))))
 
             insert_paths_SQL += [{'circuit': circuit,
                                   'symmpath_id': symmpath_id,
@@ -253,4 +288,3 @@ def convert_symmpaths_SQLite(circuit):
 # convert_symmpaths_SQLite('c1908')
 # convert_symmpaths_SQLite('c2670')
 # convert_symmpaths_SQLite('c5315')
-
